9e85c078249bce2cefe89af3fc5b6691d8cf91fc
jayeshjeh <jay.parmar.11169@gmail.com>
2022-07-22 15:58:35 +0530

    commit

---
+++ Introduction to Data Science in Python/Chp-1 Getting Started in Python/labels.py
@@ -0,0 +1,29 @@
+# Officer Deshaun
+# from matplotlib import pyplot as plt
+# plt.plot(deshaun.day_of_week, deshaun.hours_worked, label='Deshaun')
+#
+# # Add a label to Aditya's plot
+# plt.plot(aditya.day_of_week, aditya.hours_worked, label='Aditya')
+#
+# # Add a label to Mengfei's plot
+# plt.plot(mengfei.day_of_week, mengfei.hours_worked, label='Mengfei')
+#
+# # Add a command to make the legend display
+# ____
+#
+# # Display plot
+
+
+# plt.show()
+
+# Add a command to make the legend display
+# plt.legend()   # to show the names along with lines in graph
+
+# Create plot
+# plt.plot(six_months.month, six_months.hours_worked)
+#
+# # Add annotation "Missing June data" at (2.5, 80)
+# plt.text(2.5, 80, 'Missing June data')
+#
+# # Display graph
+# plt.show()
