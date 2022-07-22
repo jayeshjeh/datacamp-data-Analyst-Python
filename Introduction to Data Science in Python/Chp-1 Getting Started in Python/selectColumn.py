3b9bb629801e61024383d1fc4dd624617384ebf5
jayeshjeh <jay.parmar.11169@gmail.com>
2022-07-22 14:10:29 +0530

    commit

--- Introduction to Data Science in Python/Chp-1 Getting Started in Python/selectColumn.py
+++ Introduction to Data Science in Python/Chp-1 Getting Started in Python/selectColumn.py
@@ -1,6 +1,7 @@
 # # Select the column item from credit_records
 # # Use brackets and string notation
 # items = credit_records['item']
+# items = credit_records.item
 #
 # # Display the results
 # print(items)
