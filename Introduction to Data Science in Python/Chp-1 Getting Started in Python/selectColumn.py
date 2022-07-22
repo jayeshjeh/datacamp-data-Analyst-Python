097a58807e2334c98e13651d53be7a1869f480f5
jayeshjeh <jay.parmar.11169@gmail.com>
2022-07-22 14:13:51 +0530

    commit

--- Introduction to Data Science in Python/Chp-1 Getting Started in Python/selectColumn.py
+++ Introduction to Data Science in Python/Chp-1 Getting Started in Python/selectColumn.py
@@ -7,14 +7,3 @@
 # print(items)
 
 
-# # One or more lines of code contain errors.
-# # Fix the errors so that the code runs.
-#
-# # Select the location column in credit_records
-# location = credit_records['location']
-#
-# # Select the item column in credit_records
-# items = credit_records.item
-#
-# # Display results
-# print(location)
